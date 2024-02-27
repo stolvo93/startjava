2ced3316cc5a44d9840034bf8dd7e51a69ba6265
stolvo <stolvo.93@gmail.com>
2024-02-28 02:09:07 +0300

    Улучшена программа Calculator

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,56 +2,44 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("\n1. Калькулятор");
 
-        double a = 43;
-        char sign = '%';
-        double b = 3;
-        double result = 1;
-        boolean isSignValid = true;
-        boolean hasDivisionByZero = false;
+        int a = 16_777_216;
+        char sign = '/';
+        int b = 65_536;
+        int result = 1;
 
-        switch (sign) {
-            case '+':
-                result = a + b;
-                break;
-            case '-':
-                result = a - b;
-                break;
-            case '*':
-                result = a * b;
-                break;
-            case '/':
-                if (b == 0) {
-                    hasDivisionByZero = true;
-                    System.out.println("Деление на ноль запрещено.");
+        if (sign == '+') {
+            result = a + b;
+        } else if (sign == '-') {
+            result = a - b;
+        } else if (sign == '*') {
+            result = a * b;
+        } else if (sign == '/') {
+            if (b == 0) {
+                System.out.println("Деление на ноль запрещено.");
+            } else {
+                double divisionResult = (double) a / (double) b;
+                // не выводим дробную часть результата, если это целое число
+                if (divisionResult * 10 % 10 == 0) {
+                    System.out.printf("%d %s %d = %.0f%n", a, sign, b, divisionResult);
+                } else {
+                    System.out.println(a + " " + sign + " " + b + " = " + divisionResult);
                 }
-                result = a / b;
-                break;
-            case '^':
-                if (b > 0) {
-                    for (int i = 0; i < b; i++) {
-                        result *= a;
-                    }
-                } else if (b < 0) {
-                    for (int i = 0; i > b; i--) {
-                        result /= a;
-                    }
-                }
-                break;
-            case '%':
-                if (b == 0) {
-                    hasDivisionByZero = true;
-                    System.out.println("Деление на ноль запрещено.");
-                }
-                result = a % b;
-                break;
-            default:
-                isSignValid = false;
-                System.out.println("Неверный знак. Используйте один из следующих знаков: +, -, *, /, ^, %");
-                break;
+            }
+            return;
+        } else if (sign == '^') {
+            for (int i = 0; i < b; i++) {
+                result *= a;
+            }
+        } else if (sign == '%') {
+            if (b == 0) {
+                System.out.println("Деление на ноль запрещено.");
+                return;
+            }
+            result = a % b;
+        } else {
+            System.out.println("Неверный знак. Используйте один из следующих знаков: +, -, *, /, ^, %");
         }
 
-        if (isSignValid & !hasDivisionByZero) {
-            System.out.println(a + " " + sign + " " + b + " = " + result);
-        }
+        System.out.println(a + " " + sign + " " + b + " = " + result);
     }
 }
