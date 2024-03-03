51a32956221464117a42ddc445dc30188efbd006
stolvo <stolvo.93@gmail.com>
2024-03-04 02:21:50 +0300

    Создан класс Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "Олег";
+    String sex = "Male";
+    byte age = 27;
+    short height = 180;
+    float weight = 77.6f;
+
+    public void walk() {
+        System.out.println(name + " is walking.");
+    }
+
+    public void sit() {
+        System.out.println(name + " is sitting.");
+    }
+
+    public void run() {
+        System.out.println(name + " is running.");
+    }
+
+    public void talk() {
+        System.out.println(name + " is talking.");
+    }
+
+    public void learnJava() {
+        System.out.println(name + " is learning Java.");
+    }
+}
