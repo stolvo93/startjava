e83ba6c680aafe9f4e8c42011643fdc5a0c5ac52
stolvo <stolvo.93@gmail.com>
2024-03-06 07:39:35 +0300

    Модицицирован класс Wolf
    - Все поля теперь имеют модификатор private
    - Созданы геттеры и сеттеры
    - В сеттере age реализована проверка на корректность устанавливаемого возраста

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,72 @@
+public class Wolf {
+    
+    private String sex;
+    private String name;
+    private float weight;
+    private byte age;
+    private String coatColor;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getCoatColor() {
+        return coatColor;
+    }
+
+    public void setCoatColor() {
+        this.coatColor = coatColor;
+    }
+
+    public void walk() {
+        System.out.println("Идёт");
+    }
+
+    public void sit() {
+        System.out.println("Сидит");
+    }
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void howl() {
+        System.out.println("Воет");
+    }
+
+    public void hunt() {
+        System.out.println("Охотится");
+    }
+}
