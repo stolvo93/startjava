09cf348bd18137a8b671bb61c9ca3c9286137cbf
stolvo <stolvo.93@gmail.com>
2024-03-08 02:55:43 +0300

    Добавлены геттеры и сеттеры в класс Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,11 +1,77 @@
 public class Jaeger {
     
-    Date launchDate;
-    String mark;
-    String status;
-    double height;
-    int weight;
-    String bodyLanguage;
-    int countKaijuKilled;
+    private String modelName;
+    private Date launched;
+    private String mark;
+    private String status;
+    private double height;
+    private int weight;
+    private String bodyLanguage;
+    private int countKaijuKilled;
     
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setlaunched(Date launched) {
+        this.launched = new Date(launched.getTime());
+    }
+
+    public Date getlaunched() {
+        return new Date(launched.getTime());
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setBodyLanguage(String bodyLanguage) {
+        this.bodyLanguage = bodyLanguage;
+    }
+
+    public String getBodyLanguage() {
+        return bodyLanguage;
+    }
+
+    public void setCountKaijuKilled(int countKaijuKilled) {
+        this.countKaijuKilled = countKaijuKilled;
+    }
+
+    public int getCountKaijuKilled() {
+        return countKaijuKilled;
+    }
+
+
 }
