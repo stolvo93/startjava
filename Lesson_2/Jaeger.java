3e04804d5dfa5fabbd0570e2721909a3b0433e86
stolvo <stolvo.93@gmail.com>
2024-03-09 16:25:08 +0300

    Выполнено задание 2 итоговых ДЗ для Lesson_2

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,77 +1,78 @@
+import java.util.Locale;
+
 public class Jaeger {
     
     private String modelName;
-    private Date launched;
     private String mark;
     private String status;
     private double height;
     private int weight;
-    private String bodyLanguage;
-    private int countKaijuKilled;
     
-    public void setModelName(String modelName) {
+    public Jaeger() {}
+
+    public Jaeger(String modelName, String mark, String status, double height, int weight) {
         this.modelName = modelName;
+        this.mark = mark;
+        this.status = status;
+        setHeight(height);
+        setWeight(weight);
     }
 
     public String getModelName() {
         return modelName;
     }
 
-    public void setlaunched(Date launched) {
-        this.launched = new Date(launched.getTime());
-    }
-
-    public Date getlaunched() {
-        return new Date(launched.getTime());
-    }
-
-    public void setMark(String mark) {
-        this.mark = mark;
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
     }
 
     public String getMark() {
         return mark;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setMark(String mark) {
+        this.mark = mark;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public void setHeight(double height) {
-        this.height = height;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public double getHeight() {
         return height;
     }
 
-    public void setWeight(int weight) {
-        this.weight = weight;
+    public void setHeight(double height) {
+        if (isPositive(height, "Height")) {
+            this.height = height;
+        }
     }
 
     public int getWeight() {
         return weight;
     }
 
-    public void setBodyLanguage(String bodyLanguage) {
-        this.bodyLanguage = bodyLanguage;
+    public void setWeight(int weight) {
+        if (isPositive(weight, "Weight")) {
+            this.weight = weight;
+        }
     }
 
-    public String getBodyLanguage() {
-        return bodyLanguage;
+    public boolean isPositive (double quantifiableValue, String fieldName) {
+        if (quantifiableValue > 0) {
+            return true;
+        }
+        System.out.printf("Неверное значение %s! Используйте только положительные значения.%n%n", fieldName);
+        return false;
     }
 
-    public void setCountKaijuKilled(int countKaijuKilled) {
-        this.countKaijuKilled = countKaijuKilled;
+    @Override
+    public String toString() {
+        String s = "Name:\t%s%nMark:\t%s%nStatus: %s%nHeight: %.2f m%nWeight: %d tons%n";
+        return String.format(Locale.UK, s, modelName, mark, status, height, weight);
     }
-
-    public int getCountKaijuKilled() {
-        return countKaijuKilled;
-    }
-
-
 }
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,40 @@
+public class JaegerTest {
+    
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger();
+
+        System.out.println("Объект jaeger1 после создания при помощи конструктора без аргументов:");
+        System.out.println("Name:\t" + jaeger1.getModelName());
+        System.out.println("Mark:\t" + jaeger1.getMark());
+        System.out.println("Status: " + jaeger1.getStatus());
+        System.out.println("Height: " + jaeger1.getHeight() + " m");
+        System.out.println("Weight: " + jaeger1.getWeight() + " tons\n");
+
+        jaeger1.setModelName("Coyote Tango");
+        jaeger1.setMark("Mark-1");
+        jaeger1.setStatus("Active");
+        jaeger1.setHeight(85.34);
+        jaeger1.setWeight(2326);
+
+        System.out.println("Объект jaeger1 после присвоения его полям значений с помощью сеттеров:");
+        System.out.println(jaeger1);
+
+        jaeger1.setStatus("Destroyed");
+        jaeger1.setWeight(52544);
+
+        System.out.println("Объект jaeger1 после изменения значений Status и Weight:");
+        System.out.println(jaeger1);
+
+        Jaeger jaeger2 = new Jaeger("Striker Eureka", "Mark-5", "Active", 76.20, 2087);
+
+        System.out.println("Объект jaeger2 после инициализации при помощи параметризованного конструктора:");
+        System.out.println(jaeger2);
+
+        jaeger2.setHeight(-100);
+        jaeger2.setWeight(0);
+
+        System.out.println("Height и Weight jaeger2 после попытки присвоения неположительных значений:");
+        System.out.println("Height: " + jaeger2.getHeight() + " m");
+        System.out.println("Weight: " + jaeger2.getWeight() + " tons\n");
+    }
+}
