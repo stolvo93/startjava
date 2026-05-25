f04a13fcafedfc7e50af67e1be81b7b06b4abe31
Владимир <stolvo.93@gmail.com>
2026-05-25 17:30:34 +0300

    Выполнена первая часть задания на именование переменных

---
+++ Lesson_1/VariableNamesTheme.java
@@ -0,0 +1,151 @@
+public class VariableNamesTheme {
+	public static void main(String[] args) {
+		System.out.println("\n1. РАЗНЫЕ ПЕРЕМЕННЫЕ\n");
+		
+		// Цифра
+		int digit = 3;
+		System.out.println("Цифра = " + digit);
+
+		// Сумма цифр
+		int digitSum = 3 + 9;
+		System.out.println("Сумма чисел = " + digitSum);
+
+		// Произведение цифр
+		int digitProduct = 3 * 9;
+		System.out.println("Произведение цифр = " + digitProduct);
+
+		// Максимальное число
+		int maxNumber = 255;
+		System.out.println("Максимальное число = " + maxNumber);
+
+		// Количество десятков
+		int tensQuantity = 3;
+		System.out.println("Количество десятков = " + tensQuantity);
+
+		// Вес собаки
+		int dogWeight = 3;
+		System.out.println("Вес собаки: " + dogWeight + " кг");
+
+		// Исходное число
+		int originalNumber = 3;
+		System.out.println("Исходное число = " + originalNumber);
+
+		// Процент по вкладу
+		float depositInterest = 13.9f;
+		System.out.println("Процент по вкладу: " + depositInterest + "% годовых");
+
+		// Символ '&'
+		char ampersand = '&';
+		System.out.println("Символ '&': " + ampersand);
+
+		// Код ошибки
+		String errorCode = "\"STOP 0x0000007B\"";
+		System.out.println("Код ошибки: " + errorCode);
+
+		// Тип сообщения
+		String messageType = "Request";
+		System.out.println("Тип сообщения: " + messageType);
+
+		// Количество нулей
+		int zerosQuantity = 3;
+		System.out.println("Количество нулей = " + zerosQuantity);
+
+		// Уникальное число
+		int uniqueNumber = 4;
+		System.out.println("Уникальное число = " + uniqueNumber);
+
+		// Случайное число
+		int randomNumber = 0;
+		System.out.println("Случайное число = " + randomNumber);
+
+		// Математическое выражение
+		String mathExpression = "a + 2b";
+		System.out.println("Математическое выражение: " + mathExpression);
+
+		// Счёт в игре
+		int score = 56;
+		System.out.println("Счёт: " + score);
+
+		// Максимальная длина
+		int maxLength = 10;
+		System.out.println("Максимальная длина = " + maxLength);
+
+		// Пункт меню
+		String menuItem = "Settings";
+		System.out.println("Пункт меню: " + menuItem);
+
+		// Стоимость кофе
+		float coffePrice = 10.99f;
+		System.out.println("Стоимость кофе: $" + coffePrice);
+
+		// Начальная дата
+		String startDate = "25.05.2026";
+		System.out.println("Начальная дата: " + startDate);
+
+		// Окончание диапазона
+		int rangeEnd = 38;
+		System.out.println("Окончание диапазона = " + rangeEnd);
+
+		// Имя работника месяца
+		String employeeOfTheMonthName = "Иван Иванов";
+		System.out.println("Имя работника месяца: " + employeeOfTheMonthName);
+
+		// Название электронной книги
+		String eBookTitle = "\"Почему одни страны богатые, а другие бедные\"";
+		System.out.println("Название электронной книги: " + eBookTitle);
+
+		// Размер
+		String size = "XL";
+		System.out.println("Размер: " + size);
+
+		// Вместимость
+		float capacity = 32f;
+		System.out.println("Вместимость = " + capacity + " литра");
+
+		// Счётчик
+		int i = 3;
+		System.out.println("Счётчик = " + i);
+
+		// Путь до файла
+		String filePath = "/etc/hosts";
+		System.out.println("Путь до файла: " + filePath);
+
+		// Количество чисел в строке
+		int stringNumbersQuantity = 3;
+		System.out.println("Количество чисел в строке = " + stringNumbersQuantity);
+
+
+
+
+
+
+/*
+		System.out.println("\n2.BOOLEAN-ПЕРЕМЕННЫЕ\n");
+
+		// Цифра
+		int digit = 3;
+		System.out.println("Цифра = " + digit)
+
+		// Цифра
+		int digit = 3;
+		System.out.println("Цифра = " + digit)
+
+		// Цифра
+		int digit = 3;
+		System.out.println("Цифра = " + digit)
+
+		// Цифра
+		int digit = 3;
+		System.out.println("Цифра = " + digit)
+
+		// Цифра
+		int digit = 3;
+		System.out.println("Цифра = " + digit)
+
+		// Цифра
+		int digit = 3;
+		System.out.println("Цифра = " + digit)
+
+		*/
+	}
+}
