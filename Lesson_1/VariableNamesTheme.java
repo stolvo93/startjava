6c0cb23eac4284b4713791edd93f136d05e449d6
Владимир <stolvo.93@gmail.com>
2026-05-26 17:44:39 +0300

    Выполнена вторая часть задания на именование переменных

--- Lesson_1/VariableNamesTheme.java
+++ Lesson_1/VariableNamesTheme.java
@@ -1,151 +1,157 @@
 public class VariableNamesTheme {
-	public static void main(String[] args) {
-		System.out.println("\n1. РАЗНЫЕ ПЕРЕМЕННЫЕ\n");
-		
-		// Цифра
-		int digit = 3;
-		System.out.println("Цифра = " + digit);
+    public static void main(String[] args) {
+        System.out.println("\n1. РАЗНЫЕ ПЕРЕМЕННЫЕ\n");
+        
+        // Цифра
+        int digit = 3;
+        System.out.println("Цифра = " + digit);
 
-		// Сумма цифр
-		int digitSum = 3 + 9;
-		System.out.println("Сумма чисел = " + digitSum);
+        // Сумма цифр
+        int digitSum = 3 + 9;
+        System.out.println("Сумма чисел = " + digitSum);
 
-		// Произведение цифр
-		int digitProduct = 3 * 9;
-		System.out.println("Произведение цифр = " + digitProduct);
+        // Произведение цифр
+        int digitProduct = 3 * 9;
+        System.out.println("Произведение цифр = " + digitProduct);
 
-		// Максимальное число
-		int maxNumber = 255;
-		System.out.println("Максимальное число = " + maxNumber);
+        // Максимальное число
+        int maxNumber = 255;
+        System.out.println("Максимальное число = " + maxNumber);
 
-		// Количество десятков
-		int tensQuantity = 3;
-		System.out.println("Количество десятков = " + tensQuantity);
+        // Количество десятков
+        int tensQuantity = 3;
+        System.out.println("Количество десятков = " + tensQuantity);
 
-		// Вес собаки
-		int dogWeight = 3;
-		System.out.println("Вес собаки: " + dogWeight + " кг");
+        // Вес собаки
+        int dogWeight = 3;
+        System.out.println("Вес собаки: " + dogWeight + " кг");
 
-		// Исходное число
-		int originalNumber = 3;
-		System.out.println("Исходное число = " + originalNumber);
+        // Исходное число
+        int originalNumber = 3;
+        System.out.println("Исходное число = " + originalNumber);
 
-		// Процент по вкладу
-		float depositInterest = 13.9f;
-		System.out.println("Процент по вкладу: " + depositInterest + "% годовых");
+        // Процент по вкладу
+        float depositInterest = 13.9f;
+        System.out.println("Процент по вкладу: " + depositInterest + "% годовых");
 
-		// Символ '&'
-		char ampersand = '&';
-		System.out.println("Символ '&': " + ampersand);
+        // Символ '&'
+        char ampersand = '&';
+        System.out.println("Символ '&': " + ampersand);
 
-		// Код ошибки
-		String errorCode = "\"STOP 0x0000007B\"";
-		System.out.println("Код ошибки: " + errorCode);
+        // Код ошибки
+        String errorCode = "\"STOP 0x0000007B\"";
+        System.out.println("Код ошибки: " + errorCode);
 
-		// Тип сообщения
-		String messageType = "Request";
-		System.out.println("Тип сообщения: " + messageType);
+        // Тип сообщения
+        String messageType = "Request";
+        System.out.println("Тип сообщения: " + messageType);
 
-		// Количество нулей
-		int zerosQuantity = 3;
-		System.out.println("Количество нулей = " + zerosQuantity);
+        // Количество нулей
+        int zerosQuantity = 3;
+        System.out.println("Количество нулей = " + zerosQuantity);
 
-		// Уникальное число
-		int uniqueNumber = 4;
-		System.out.println("Уникальное число = " + uniqueNumber);
+        // Уникальное число
+        int uniqueNumber = 4;
+        System.out.println("Уникальное число = " + uniqueNumber);
 
-		// Случайное число
-		int randomNumber = 0;
-		System.out.println("Случайное число = " + randomNumber);
+        // Случайное число
+        int randomNumber = 0;
+        System.out.println("Случайное число = " + randomNumber);
 
-		// Математическое выражение
-		String mathExpression = "a + 2b";
-		System.out.println("Математическое выражение: " + mathExpression);
+        // Математическое выражение
+        String mathExpression = "a + 2b";
+        System.out.println("Математическое выражение: " + mathExpression);
 
-		// Счёт в игре
-		int score = 56;
-		System.out.println("Счёт: " + score);
+        // Счёт в игре
+        int score = 56;
+        System.out.println("Счёт: " + score);
 
-		// Максимальная длина
-		int maxLength = 10;
-		System.out.println("Максимальная длина = " + maxLength);
+        // Максимальная длина
+        int maxLength = 10;
+        System.out.println("Максимальная длина = " + maxLength);
 
-		// Пункт меню
-		String menuItem = "Settings";
-		System.out.println("Пункт меню: " + menuItem);
+        // Пункт меню
+        String menuItem = "Settings";
+        System.out.println("Пункт меню: " + menuItem);
 
-		// Стоимость кофе
-		float coffePrice = 10.99f;
-		System.out.println("Стоимость кофе: $" + coffePrice);
+        // Стоимость кофе
+        float coffePrice = 10.99f;
+        System.out.println("Стоимость кофе: $" + coffePrice);
 
-		// Начальная дата
-		String startDate = "25.05.2026";
-		System.out.println("Начальная дата: " + startDate);
+        // Начальная дата
+        String startDate = "25.05.2026";
+        System.out.println("Начальная дата: " + startDate);
 
-		// Окончание диапазона
-		int rangeEnd = 38;
-		System.out.println("Окончание диапазона = " + rangeEnd);
+        // Окончание диапазона
+        int rangeEnd = 38;
+        System.out.println("Окончание диапазона = " + rangeEnd);
 
-		// Имя работника месяца
-		String employeeOfTheMonthName = "Иван Иванов";
-		System.out.println("Имя работника месяца: " + employeeOfTheMonthName);
+        // Имя работника месяца
+        String employeeOfTheMonthName = "Иван Иванов";
+        System.out.println("Имя работника месяца: " + employeeOfTheMonthName);
 
-		// Название электронной книги
-		String eBookTitle = "\"Почему одни страны богатые, а другие бедные\"";
-		System.out.println("Название электронной книги: " + eBookTitle);
+        // Название электронной книги
+        String ebookTitle = "\"Почему одни страны богатые, а другие бедные\"";
+        System.out.println("Название электронной книги: " + ebookTitle);
 
-		// Размер
-		String size = "XL";
-		System.out.println("Размер: " + size);
+        // Размер
+        String size = "XL";
+        System.out.println("Размер: " + size);
 
-		// Вместимость
-		float capacity = 32f;
-		System.out.println("Вместимость = " + capacity + " литра");
+        // Вместимость
+        float capacity = 32.5f;
+        System.out.println("Вместимость: " + capacity + " литра");
 
-		// Счётчик
-		int i = 3;
-		System.out.println("Счётчик = " + i);
+        // Счётчик
+        int i = 3;
+        System.out.println("Счётчик = " + i);
 
-		// Путь до файла
-		String filePath = "/etc/hosts";
-		System.out.println("Путь до файла: " + filePath);
+        // Путь до файла
+        String filePath = "/etc/hosts";
+        System.out.println("Путь до файла: " + filePath);
 
-		// Количество чисел в строке
-		int stringNumbersQuantity = 3;
-		System.out.println("Количество чисел в строке = " + stringNumbersQuantity);
+        // Количество чисел в строке
+        int stringNumbersQuantity = 3;
+        System.out.println("Количество чисел в строке = " + stringNumbersQuantity);
 
+        System.out.println("\n2. BOOLEAN-ПЕРЕМЕННЫЕ\n");
 
+        // Сотни равны?
+        boolean isEqual = true;
+        System.out.println("Сотни равны? " + isEqual);
 
+        // Компьютер включён?
+        boolean isOn = false;
+        System.out.println("Компьютер включён? " + isOn);
 
+        // Есть равные цифры?
+        boolean hasEqualNumbers = true;
+        System.out.println("Есть равные цифры? " + hasEqualNumbers);
 
+        // Служба создана?
+        boolean isCreated = false;
+        System.out.println("Служба создана? " + isCreated);
 
-/*
-		System.out.println("\n2.BOOLEAN-ПЕРЕМЕННЫЕ\n");
+        // Файл пустой?
+        boolean isEmpty = true;
+        System.out.println("Файл пустой? " + isEmpty);
 
-		// Цифра
-		int digit = 3;
-		System.out.println("Цифра = " + digit)
+        // Соединение активное?
+        boolean isActive = false;
+        System.out.println("Соединение активное? " + isActive);
 
-		// Цифра
-		int digit = 3;
-		System.out.println("Цифра = " + digit)
+        // Новый?
+        boolean isNew = true;
+        System.out.println("Новый? " + isNew);
 
-		// Цифра
-		int digit = 3;
-		System.out.println("Цифра = " + digit)
+        // Электронная почта действительная?
+        boolean isValid = false;
+        System.out.println("Электронная почта действительная? " + isValid);
 
-		// Цифра
-		int digit = 3;
-		System.out.println("Цифра = " + digit)
+        // Имеются уникальные строки?
+        boolean hasUniqueLines = true;
+        System.out.println("Имеются уникальные строки? " + hasUniqueLines);
 
-		// Цифра
-		int digit = 3;
-		System.out.println("Цифра = " + digit)
-
-		// Цифра
-		int digit = 3;
-		System.out.println("Цифра = " + digit)
-
-		*/
-	}
+        System.out.println("\n2. АББРЕВИАТУРЫ\n");
+    }
 }
