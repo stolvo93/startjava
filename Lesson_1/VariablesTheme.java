f5c36eb51e7d654aed8220bba787276d5cf4e1f1
Владимир <stolvo.93@gmail.com>
2026-05-28 17:48:45 +0300

    Выполнена первая часть задания на вывод ASCII-графики

---
+++ Lesson_1/VariablesTheme.java
@@ -0,0 +1,12 @@
+public class VariablesTheme {
+    public static void main(String[] args) {
+        System.out.println("\n1. ВЫВОД ASCII ГРАФИКИ\n");
+
+        System.out.println(String.join("\n",
+                "                     /\\",
+                "   J    a  v     v  /  \\",
+                "   J   a a  v   v  /_( )\\",
+                "J  J  aaaaa  V V  /      \\",
+                " JJ  a     a  V  /___/\\___\\"));
+    }
+}
