db275643bb7246ab8029c6bbb2f44ff2ab57c825
stolvo <stolvo.93@gmail.com>
2026-06-13 11:46:29 +0300

    Создан класс Person по заданию урока 2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person extends {
+
+    String sex = "female";
+    String name = "Карина";
+    int height = 168;
+    float weight = 44.2;
+    int age = 25;
+
+    void walk() {
+        System.out.println(name + " идёт");
+    }
+
+    void sit() {
+        System.out.println(name + " сидит");
+    }
+
+    void run() {
+        System.out.println(name + " бежит");
+    }
+
+    void talk() {
+        System.out.println(name + " говорит");
+    }
+
+    void learnJava() {
+        System.out.println(name + " изучает Java");
+    }
+}
