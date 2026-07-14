dfa6bc52108570bb61584351281470b3e9d34ba8
Владимир <stolvo.93@gmail.com>
2026-07-14 14:57:23 +0300

    UniqueNumbersArray: Исправлен баг вывода сообщений об ошибке. Проведен рефакторинг

--- src/com/startjava/lesson_2_3_4/array/UniqueNumbersArray.java
+++ src/com/startjava/lesson_2_3_4/array/UniqueNumbersArray.java
@@ -5,16 +5,16 @@ import java.util.Random;
 
 public class UniqueNumbersArray {
     public static void main(String[] args) {
-        int[][] inputValues = {
+        int[][] argumentSets = {
                 {-30, -10, 23},
                 {10, 50, 10},
                 {-34, -34, 1},
                 {-1, 2, -3},
                 {5, -8, 2},
         };
-        for (int[] values : inputValues) {
+        for (int[] argumentSet : argumentSets) {
             System.out.println();
-            print(values[0], values[1], values[2]);
+            print(argumentSet[0], argumentSet[1], argumentSet[2]);
         }
     }
 
@@ -23,18 +23,18 @@ public class UniqueNumbersArray {
             printInvalidLineLengthError(lineLength);
             return;
         }
-
+        if (leftBound > rightBound) {
+            printLeftBoundIsGreaterError(leftBound, rightBound);
+            return;
+        }
         int rangeSize = rightBound - leftBound + 1;
         int arrayLength = rangeSize * 3 / 4;
         if (arrayLength <= 0) {
             printInvalidArrayLength(arrayLength);
-            if (leftBound > rightBound) {
-                printLeftBoundIsGreaterError(leftBound, rightBound);
-            }
             return;
         }
 
-        int[] uniqueNumbers = generateUniqueNumbersArray(leftBound, rightBound);
+        int[] uniqueNumbers = generateUniqueNumbersArray(leftBound, rangeSize, arrayLength);
         printSortedNumbers(uniqueNumbers, lineLength);
     }
 
@@ -42,25 +42,23 @@ public class UniqueNumbersArray {
         System.out.printf("Ошибка: количество чисел в строке не должно быть < 1 (%d)%n", lineLength);
     }
 
-    private static void printInvalidArrayLength(int arrayLength) {
-        System.out.printf("Ошибка: длина массива должна быть > 0 (%d)%n", arrayLength);
-    }
-
     private static void printLeftBoundIsGreaterError(int leftBound, int rightBound) {
         System.out.printf("Ошибка: левая граница (%d) > правой (%d)%n", leftBound, rightBound);
     }
 
-    private static int[] generateUniqueNumbersArray(int leftBound, int rightBound) {
-        int rangeSize = rightBound - leftBound + 1;
-        int length = rangeSize * 3 / 4;
-        int[] randomUniqueNumbers = new int[length];
-        Random random = new Random();
+    private static void printInvalidArrayLength(int arrayLength) {
+        System.out.printf("Ошибка: длина массива должна быть > 0 (%d)%n", arrayLength);
+    }
+
+    private static int[] generateUniqueNumbersArray(int minValue, int rangeSize, int arrayLength) {
+        int[] randomUniqueNumbers = new int[arrayLength];
         int uniqueNumbersCount = 0;
-        while (uniqueNumbersCount < length) {
-            int newNumber = random.nextInt(rangeSize) + leftBound;
+        Random random = new Random();
+        while (uniqueNumbersCount < arrayLength) {
+            int newNumber = random.nextInt(rangeSize) + minValue;
             boolean isDuplicate = false;
             for (int i = 0; i < uniqueNumbersCount; i++) {
-                if (randomUniqueNumbers[i] == newNumber) {
+                if (newNumber == randomUniqueNumbers[i]) {
                     isDuplicate = true;
                     break;
                 }
@@ -78,7 +76,7 @@ public class UniqueNumbersArray {
         Arrays.sort(numbers);
         StringBuilder sb = new StringBuilder("Числа, отсортированные по возрастанию:\n");
         for (int i = 0; i < numbers.length - 1; i++) {
-            sb.append(numbers[i]).append(", ");
+            sb.append(numbers[i]).append(" ");
             if ((i + 1) % lineLength == 0) {
                 sb.append("\n");
             }
