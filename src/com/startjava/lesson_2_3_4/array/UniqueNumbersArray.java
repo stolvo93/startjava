4d2c8673d2d2ac6187b5cb625ff0e206e6d9a219
Владимир <stolvo.93@gmail.com>
2026-07-17 16:43:53 +0300

    Доработан UniqueNumbersArray

--- src/com/startjava/lesson_2_3_4/array/UniqueNumbersArray.java
+++ src/com/startjava/lesson_2_3_4/array/UniqueNumbersArray.java
@@ -11,7 +11,11 @@ public class UniqueNumbersArray {
                 {-34, -34, 1},
                 {-1, 2, -3},
                 {5, -8, 2},
+                {5, -8, -3},
+                {1, 1, -1},
+                {-150, 20, 20}
         };
+
         for (int[] argumentSet : argumentSets) {
             System.out.println();
             print(argumentSet[0], argumentSet[1], argumentSet[2]);
@@ -19,18 +23,23 @@ public class UniqueNumbersArray {
     }
 
     private static void print(int leftBound, int rightBound, int lineLength) {
-        if (lineLength < 1) {
-            printInvalidLineLengthError(lineLength);
-            return;
-        }
         if (leftBound > rightBound) {
             printLeftBoundIsGreaterError(leftBound, rightBound);
+        }
+        if (lineLength < 1) {
+            printInvalidLineLengthError(lineLength);
+        }
+        if (leftBound > rightBound) {
             return;
         }
+
         int rangeSize = rightBound - leftBound + 1;
         int arrayLength = rangeSize * 3 / 4;
         if (arrayLength <= 0) {
-            printInvalidArrayLength(arrayLength);
+            printInvalidArrayLengthError(arrayLength);
+            return;
+        }
+        if (lineLength < 1) {
             return;
         }
 
@@ -46,7 +55,7 @@ public class UniqueNumbersArray {
         System.out.printf("Ошибка: левая граница (%d) > правой (%d)%n", leftBound, rightBound);
     }
 
-    private static void printInvalidArrayLength(int arrayLength) {
+    private static void printInvalidArrayLengthError(int arrayLength) {
         System.out.printf("Ошибка: длина массива должна быть > 0 (%d)%n", arrayLength);
     }
 
@@ -75,13 +84,29 @@ public class UniqueNumbersArray {
     private static void printSortedNumbers(int[] numbers, int lineLength) {
         Arrays.sort(numbers);
         StringBuilder sb = new StringBuilder("Числа, отсортированные по возрастанию:\n");
-        for (int i = 0; i < numbers.length - 1; i++) {
-            sb.append(numbers[i]).append(" ");
+        int lastIndex = numbers.length - 1;
+        int firstNumberSize = determineNumberSize(numbers[0]);
+        int lastNumberSize = determineNumberSize(numbers[lastIndex]);
+        int numbersMaxSize = Math.max(firstNumberSize, lastNumberSize);
+        String numberFormat = String.format("%%%dd ", numbersMaxSize);
+        for (int i = 0; i <= lastIndex; i++) {
+            sb.append(String.format(numberFormat, numbers[i]));
+            if (i == lastIndex) {
+                continue;
+            }
             if ((i + 1) % lineLength == 0) {
-                sb.append("\n");
+                sb.append('\n');
             }
         }
-        sb.append(numbers[numbers.length - 1]);
         System.out.println(sb);
+    }
+
+    private static int determineNumberSize(int number) {
+        int size = number >= 0 ? 1 : 2;
+        while (number / 10 != 0) {
+            number /= 10;
+            size++;
+        }
+        return size;
     }
 }
