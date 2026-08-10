bbcf441b6d08fc5ff585d9234ac9e42aa8ead875
stolvo <stolvo.93@gmail.com>
2026-08-11 00:11:44 +0300

    Удален неиспользуемый класс исключения

--- src/com/startjava/lesson_2_3_4/guess/NumberNotInRangeException.java
+++
@@ -1,7 +0,0 @@
-package com.startjava.lesson_2_3_4.guess;
-
-public class NumberNotInRangeException extends RuntimeException {
-    public NumberNotInRangeException(String message) {
-        super(message);
-    }
-}
